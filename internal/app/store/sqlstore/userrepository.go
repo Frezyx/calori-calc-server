b59ff11f4323b8643e06ca3f8e1351c1931bdbd6
Stanislav <tcetyre5@gmail.com>
2020-06-13 20:45:46 +0300

    database store refactored

--- internal/app/store/sqlstore/userrepository.go
+++ internal/app/store/sqlstore/userrepository.go
@@ -1,6 +1,11 @@
-package store
+package sqlstore
 
-import "github.com/Frezyx/calory-calc-server/internal/app/model"
+import (
+	"database/sql"
+
+	"github.com/Frezyx/calory-calc-server/internal/app/model"
+	"github.com/Frezyx/calory-calc-server/internal/app/store"
+)
 
 //UserRepository ...
 type UserRepository struct {
@@ -8,22 +13,19 @@ type UserRepository struct {
 }
 
 //Create ...
-func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+func (r *UserRepository) Create(u *model.User) error {
 	if err := u.Validate(); err != nil {
-		return nil, err
+		return err
 	}
 
 	if err := u.BeforeCreate(); err != nil {
-		return nil, err
+		return err
 	}
 
-	if err := r.store.db.QueryRow("INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
+	return r.store.db.QueryRow("INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
 		u.Email,
 		u.EncryptedPassword,
-	).Scan(&u.ID); err != nil {
-		return nil, err
-	}
-	return u, nil
+	).Scan(&u.ID)
 }
 
 //FindByEmail ...
@@ -36,6 +38,9 @@ func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
 		&u.Email,
 		&u.EncryptedPassword,
 	); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, store.ErrRecordNotFound
+		}
 		return nil, err
 	}
 	return u, nil
---
+++ internal/app/store/sqlstore/userrepository_test.go
@@ -0,0 +1,37 @@
+package sqlstore_test
+
+import (
+	"testing"
+
+	"github.com/Frezyx/calory-calc-server/internal/app/model"
+	"github.com/Frezyx/calory-calc-server/internal/app/store"
+	"github.com/Frezyx/calory-calc-server/internal/app/store/sqlstore"
+	"github.com/stretchr/testify/assert"
+)
+
+//Not work
+
+func TestUserRepository_Create(t *testing.T) {
+	db, teardown := sqlstore.TestDB(t, databaseURL)
+	defer teardown("users")
+
+	s := sqlstore.New(db)
+	u := model.TestUser(t)
+	assert.NoError(t, s.User().Create(u))
+	assert.NotNil(t, u.ID)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	db, teardown := sqlstore.TestDB(t, databaseURL)
+	defer teardown("users")
+
+	s := sqlstore.New(db)
+	u1 := model.TestUser(t)
+	_, err := s.User().FindByEmail(u1.Email)
+	assert.EqualError(t, err, store.ErrRecordNotFound.Error())
+
+	s.User().Create(u1)
+	u2, err := s.User().FindByEmail(u1.Email)
+	assert.NoError(t, err)
+	assert.NotNil(t, u2)
+}
